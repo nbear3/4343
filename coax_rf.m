3fb31b40d833641e10bddcbfbbd7067d5f26006d
Hong-Hanh Nguyen <hnguyen303@gatech.edu>
2018-04-04 12:21:01 -0400

    Added BEMT

--- coax_rf.m
+++ coax_rf.m
@@ -8,7 +8,7 @@ clc
 clear
 
 n = 2; % per rotor
-c = 0.5;
+c = .8;
 R_base = 4.5; 
 
 % Constants 
@@ -16,9 +16,15 @@ GW_base = 600*2.2046;
 A_base = pi*R_base^2;
 
 coax.DL = GW_base/(A_base*2);
-coax.sigma = 2*n*c/(pi*R_base);
-coax.Vtip = 685;
-coax.losses = 0.1;
+% coax.sigma = 2n*c/(pi*R_base);
+coax.OR = 685;
+% coax.losses = 0.1;
+coax.t = -9;
+coax.R = R_base;
+coax.c = c;
+coax.tr = .8;
+coax.n = n;
+coax.airfoil = 'sc1095.xlsx';
 
 [t, MCP] = hover_time(coax);
 fprintf('Coax\n')
@@ -42,7 +48,7 @@ function [t, MCP] = hover_time(rotor)
     total_payload = 100*2.2046;
 
     % Performance Requirements
-    time_step = .1;
+    time_step = 1000;
     hover = struct('V', 0, 'V_c', 0, 't', time_step, 'h', 3000*3.28, 'W', GW);
     [~, ~, P1_sl] = phase_calc(GW, hover.W, 0, rotor, hover);
     MCP = P1_sl;
@@ -54,7 +60,7 @@ function [t, MCP] = hover_time(rotor)
     gamma = MCP./MCP_base;
     sfc = sfc_base * (-.00932*gamma.^2+.865*gamma+.445)./(gamma+.301);
 
-    BL = GW/(density(0)*GW/rotor.DL*rotor.Vtip^2)/rotor.sigma;
+%     BL = GW/(density(0)*GW/rotor.DL*rotor.Vtip^2)/rotor.sigma;
     
     t = 0;
     W = GW;    
@@ -63,53 +69,40 @@ function [t, MCP] = hover_time(rotor)
         F_req = phase_calc(GW, W, sfc, rotor, hover);
         W = W - F_req;
         Rf_avail = Rf_avail - F_req/GW;
-        t = t + time_step;
+        t = t + time_step
     end
 end
 
 %% Helper Functions
 function [F_req, P_req, P_req_sl] = phase_calc(GW, W, sfc, rotor, phase)
     
-    Cd_o = 0.008;
-    k = 1.15;
-    DL = rotor.DL;
-    sigma = rotor.sigma;
-    Vtip = rotor.Vtip;
-    
-    A = GW/DL;
-    f = -2e-8*GW.^2 + 0.0011*GW + 1.9143; % ft2
+%     Cd_o = 0.008;
+%     k = 1.15;
+%     DL = rotor.DL;
+%     
+%     A = GW/DL;
+%     f = -2e-8*GW.^2 + 0.0011*GW + 1.9143; % ft2
 
     % Phase Parameters
     V = phase.V;
-    V_c = phase.V_c;
     hours = phase.t/3600;
-    rho = density(phase.h);
     
-    % Calculation
-    D = .5*rho*V^2*f;
-    alpha = atan(D./W);
-    T = sqrt(D.^2+W.^2);
-    Ct = T./(rho*A*Vtip^2);
-    mu = V.*cos(alpha)/Vtip;
-    lambda = (V_c/2+sqrt((V_c/2).^2+DL/(2*rho)))/(Vtip);
-    Ku = sqrt((-(mu./lambda).^2+sqrt((mu./lambda).^4+4))/2);
-    Cp_i = k*Ct.*lambda.*Ku;
-    Cp_o = sigma*Cd_o/8*(1+4.65*mu.^2);
-    Cp_p = .5*f.*(mu.^3)./A;
-    Cp = Cp_i + Cp_o + Cp_p;
-    Cp_tail = Cp * rotor.losses;
-    
-    P_req = (Cp+Cp_tail).*(rho*A*(Vtip)^3)/550; % in HP
-    P_req_sl = P_req*density(0)/rho;
+    Thrust = 0;
+    thr = 0;
+    while Thrust < W
+        [Thrust, P_req, P_req_sl, FM] = CoaxBEMT(rotor, thr, phase.h, phase.V_c);
+        thr = thr + .1;
+       
+    end
     
     % Update weight
     F_req = P_req*hours.*sfc;
 end
 
-function rho = density(h)
-    % Calculate Desnity
-    rho = .00238*(1-.00198*h/288.16)^4.2553;
-end
+% function rho = density(h)
+%     % Calculate Desnity
+%     rho = .00238*(1-.00198*h/288.16)^4.2553;
+% end
 
 function W_engine = engine_weight(MCP) 
     W_engine = (.1054*MCP.^2+358*MCP+2.757e4)./(MCP+1180);
