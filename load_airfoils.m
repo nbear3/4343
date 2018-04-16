923e1dcaaf8f3387ddc7220787c06223d4901f0e
nbear3 <jcbutter@gmail.com>
2018-04-15 20:46:22 -0400

    fixed load airfoils

--- load_airfoils.m
+++ load_airfoils.m
@@ -1,9 +1,9 @@
-
-
-files = dir('airfoils/*.xlsx');
-r = size(files);
-airfoil = cell(1, r);
-for i = 1:r/2
-    airfoil(i).tip = xlsread(strcat('airfoils/', files(2*i-1).name));
-    airfoil(i).root = xlsread(strcat('airfoils/', files(2*i).name));
-end
+function airfoils = load_airfoils()
+    % load airfoils from the \airfoils directory
+    files = dir('airfoils/*.xlsx');
+    r = size(files);
+    airfoils = cell(1, r);
+    for i = 1:r/2
+        airfoils(i).root = xlsread(strcat('airfoils/', files(2*i-1).name));
+        airfoils(i).tip = xlsread(strcat('airfoils/', files(2*i).name));
+    end
