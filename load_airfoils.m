1410927179dea3f7f29c36e60e39d0ada1c291ac
nbear3 <jcbutter@gmail.com>
2018-04-15 20:52:47 -0400

    fixed load airfoils again

--- load_airfoils.m
+++ load_airfoils.m
@@ -1,8 +1,8 @@
 function airfoils = load_airfoils()
     % load airfoils from the \airfoils directory
     files = dir('airfoils/*.xlsx');
-    r = size(files);
-    airfoils = cell(1, r);
+    [r, ~] = size(files);
+    airfoils(r/2) = struct();
     for i = 1:r/2
         airfoils(i).root = xlsread(strcat('airfoils/', files(2*i-1).name));
         airfoils(i).tip = xlsread(strcat('airfoils/', files(2*i).name));
