fbd590b786b4c6634144ed975f2f29bd35f67cd3
nbear3 <jcbutter@gmail.com>
2018-04-15 22:11:22 -0400

    full airfoil analysis

--- airfoil_analysis.m
+++ airfoil_analysis.m
@@ -6,8 +6,8 @@ c = 1;
 theta_t = 10.1;
 tr = 0.9;
 
-W = 600*2.2/2;
-h = 3000*3.28;
+W = 600*2.20462;
+h = 0;
 
 intmesh.OR = 700;
 intmesh.t = theta_t;
@@ -21,16 +21,30 @@ intmesh.download = 0.9;
 
 airfoils = load_airfoils;
 
+hold on
 for i = 1:length(airfoils)
     % Odd is tip; Even is root
     airfoils(i).ratio = 0.5;
-    P_hover(i) = thrust_bemt(W, 1e-10, intmesh, h, 0, airfoils(i), 25);    
-end
+    
+    j = 1;
+    for thr = theta_t:.1:25
+        [T(j), P, P_sl, FM] = bemt(intmesh, thr, h, 0, airfoils(i));
+        CT_CQ(j) = T(j)/P/550*intmesh.OR;
+        j=j+1;
+    end
+    
+    plot(2*T, CT_CQ)
+%     P_hover(i) = thrust_bemt(W, 1e-10, intmesh, h, 0, airfoils(i), 25);    
+end 
 
+xlabel('Thrust (lbs)')
+ylabel('C_T/C_Q')
 
-
-
-
+x = [W W];
+plot(x, ylim, '--')
+plot(2*x, ylim, '--')
+legend('Huey', 'Osprey', 'K-max', 'UH-60A', 'Last Year', 'W', '2W')
+title('CT over CQ for Common Airfoil Combinations in Hover at SL')
 
 
 
