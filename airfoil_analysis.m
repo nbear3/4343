abb5ee64eff1874987fd94eef33fee1256068f01
Hong-Hanh Nguyen <hnguyen303@gatech.edu>
2018-04-17 18:16:43 -0400

    Added Re, updated airfoils

--- airfoil_analysis.m
+++ airfoil_analysis.m
@@ -2,14 +2,14 @@ clc
 clear
 close all
 
-c = .6;
-theta_t = 10.1;
-tr = 0.9;
+c = .79;
+theta_t = 5.08;
+tr = 0.47;
 
 W = 600*2.20462;
-h = 0;
+h = 3000*3.28084;
 
-intmesh.OR = 700;
+intmesh.OR = 650;
 intmesh.t = theta_t;
 intmesh.R = 1.3*3.28;
 intmesh.c = c;
@@ -24,7 +24,7 @@ airfoils = load_airfoils;
 hold on
 for i = 1:length(airfoils)
     % Odd is tip; Even is root
-    airfoils(i).ratio = 0.6;
+    airfoils(i).ratio = 0.5;
     
     j = 1;
     for thr = theta_t:.1:25
@@ -32,19 +32,21 @@ for i = 1:length(airfoils)
         CT_CQ(j) = T(j)/P/550*intmesh.OR;
         j=j+1;
     end
-    
+    T = T/2.20462;
     plot(2*T, CT_CQ)
 %     P_hover(i) = thrust_bemt(W, 1e-10, intmesh, h, 0, airfoils(i), 25);    
 end 
 
-xlabel('Thrust (lbs)')
+xlabel('Thrust (kg)')
 ylabel('C_T/C_Q')
 
-x = [W W];
+x = [W/2.20462 W/2.20462];
 plot(x, ylim, '--')
-plot(2*x, ylim, '--')
-legend('Huey', 'Osprey', 'K-max', 'UH-60A', 'Last Year', 'W', '2W')
-title('CT over CQ for Common Airfoil Combinations in Hover at SL')
+plot(1.5*x, ylim, '--')
+legend('Naca63-210 (R) & Naca23012 (T)', 'Naca 23012 (R&T)','Naca23012 (R) & VR-1 (T)', 'Naca23012 (R) & VR-1 (T)', 'RC0864C (R) & Naca23012 (T)', 'W', '1.5W')
+title('CT over CQ for Common Airfoil Combinations in Hover at 3000 m')
+%title('CT over CQ for Common Airfoil Combinations in Hover at Sea Level')
+set(findall(gca, 'Type', 'Line'),'LineWidth',1);
 
 
 
