87b2c30cacbca00e9674047c865d302b2a5114ab
nbear3 <jcbutter@gmail.com>
2018-05-02 01:30:56 -0400

    added power limit

--- flight_envelope.m
+++ flight_envelope.m
@@ -8,43 +8,38 @@ S = 5.884*(3.28084)^2;
 g = 32.17;
 Cl_max = 1.4299;
 
-V = linspace(0, 250/.592484);
+V = linspace(0, 150/.592484, 500);
 L = .5*Cl_max*rho*V.^2*S;
-n = L/W;
+n_aero = L/W;
 
-plot(V*.592484, n);
-hold on
+V = V*.592484;
+
 
 [W, intmesh, airfoil] = load_intmesh();
 h = 0;
 j = 1;
-for thr = 20:.01:40
+for thr = 25:.5:36
     [T(j), P(j), ~, ~] = bemt(intmesh, thr, h, 0, airfoil);
     j=j+1;
 end
 
-n_power = 2*interp1(P, T, 280/2)/W;
-n_aero_limit = 2*max(T)/W;
-plot(xlim, [n_power n_power])
-plot(xlim, [n_aero_limit n_aero_limit])
+[Vkts, n_power] = power_limit();
+n_power_hover = 2*interp1(P, T, 280/2)/W;
+n_rotor = 2*max(T)/W;
 
-% Vstall = sqrt(2*W/(rho*S*Cl_max))*0.592484; % knots
-% n_stall = 3.2*(rho*S*Cl_max) * (Vstall/0.592484)^2/(2*W);
+hold on
+plot(V, n_aero, '--');
+plot(Vkts, n_power, '--')
+plot(xlim, n_power_hover*ones(1, 2), '--')
+plot(xlim, [n_rotor n_rotor], '--')
+plot(xlim, [3 3], '--')
 
-% Vmax = 225;
-% 
-% num = 100; % number of iteration
-% V = linspace(0, 250/0.592484, num); % ft/s
-% 
-% for i = 1:num
-%     n(i) = (rho*S*Cl_max)*V(i)^2 / (2*W);
-% end
+i1 = find(n_aero > n_power_hover, 1);
+plot([0 V(i1)], n_power_hover*ones(1, 2), 'k', 'LineWidth', 1.5)
 
+i2 = find(n_aero > 3, 1);
+plot(V(i1:i2), n_aero(i1:i2), 'k', 'LineWidth', 1.5)
 
-% hold on
-% ylim([0,5])
-% xlim([0, 250])
-plot(xlim, [3 3])
 title('Flight Envelope')
 xlabel('V_{\infty} (kts)')
 ylabel('Load factor, n')
