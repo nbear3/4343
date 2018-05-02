b426dc1d4f0995fa3c0195bc382a6c83dd555b3c
nbear3 <jcbutter@gmail.com>
2018-05-02 16:12:33 -0400

    cleaned up perfomance print out

--- ff_performance.m
+++ ff_performance.m
@@ -3,20 +3,25 @@ clear
 close all
 
 MCP = 280;
+Dash_THP = MCP*1.1;
+
 h = [0, 3000];
 
 for n = 1:length(h)
     h_ft = h(n)*3.28084; % convert to ft
     [Vkts, P_req] = compound_power(h_ft);
+    fprintf('\nPerformance @ %d m\n', h(n));
     
     %% Plot The Max Speed
     figure
     hold on
     P_avail = MCP*density(h_ft)/density(0);
     V_max = interp1(P_req, Vkts, P_avail);
-    fprintf('\nMaximum MCP Speed @ %d m\n', h(n));
-    fprintf('\tMax Speed: %.2f kts\n', V_max);
+    V_dash = interp1(P_req, Vkts, Dash_THP);
+    fprintf('\tMax Speed @ MCP: %.2f kts\n', V_max);
     fprintf('\t@P: %.2f HP\n', P_avail);
+    fprintf('\n\tDash Speed: %.2f kts\n', V_dash);
+    fprintf('\t@P: %.2f HP\n', Dash_THP);
 
     yl = ylim;
     plot(Vkts, P_req);
@@ -26,8 +31,7 @@ for n = 1:length(h)
     
     %% Plot The Max Range
     [R, V_range, P_range] = ff_range(Vkts, P_req);
-    fprintf('\nBest Range @ %d m\n', h(n));
-    fprintf('\tTotal Range: %.2f km\n', R);
+    fprintf('\n\tTotal Range: %.2f km\n', R);
     fprintf('\t50%% Range: %.2f km\n', R/2);
     fprintf('\t@V: %.2f kts\n', V_range);
     fprintf('\t@P: %.2f HP\n', P_range);
